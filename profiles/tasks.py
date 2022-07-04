d417f9ad324f7d23e887d350daa556e9b3fefc95
theradP <mpranjal380@gmail.com>
2022-07-04 10:00:44 +0530

    fixed celery task

--- profiles/tasks.py
+++ profiles/tasks.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from .models import InstallDetails
+from celery import shared_task
 
 
+@shared_task(name="update_installs")
 def update_installs():
     file = "static/csv/installs - installs.csv"
     df = pd.read_csv(file)
     data = []
     for i in df.to_dict('records'):
-        data.append(InstallDetails**i)
+        data.append(InstallDetails(**i))
     InstallDetails.objects.bulk_create(data)
--- website/celery.py
+++ website/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import os
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
@@ -17,14 +18,11 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
+
 app.conf.beat_schedule = {
-    #Scheduler Name
     'update_install_data': {
         # Task Name (Name Specified in Decorator)
-        'task': 'profiles.',
-        # Schedule
-        'schedule': 10.0,
-        # Function Arguments
-        'args': ("Hello",)
+        'task': 'profiles.update_installs',
+        'schedule': crontab(minute='*/10'),
     },
 }
